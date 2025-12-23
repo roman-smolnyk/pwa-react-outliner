48d7cec397184286dcfeb6eafbdfaed5a7fdf016
roman-smolnyk <poma23324@gmail.com>
2025-12-23 12:30:18 +0100

    [feat] Add capacitor

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import "./App.css";
 import { TreeRoAPI } from "./api";
 import { LoginFormComponent } from "./components/authComp";
 import MainAppComponent from "./components/mainAppComp";
+import { Capacitor } from "@capacitor/core";
 
 function App() {
+  console.log("Capacitor.isNativePlatform()", Capacitor.isNativePlatform());
   TreeRoAPI.useStore((state) => state.localConfig.isAuthorized);
 
   if (!TreeRoAPI.isAuthorized()) {
