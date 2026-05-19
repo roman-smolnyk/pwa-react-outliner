bc3a457eab793cf58df50838cb8f913060c2170a
roman-smolnyk <poma23324@gmail.com>
2026-05-20 00:33:39 +0200

    [style] Floating menu style

--- src/components/Common/FloatingMenuButton.tsx
+++ src/components/Common/FloatingMenuButton.tsx
@@ -6,7 +6,7 @@ export default function FloatingMenuButton({
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & { className: string }) {
   return (
     <button
-      className={`FloatingMenuButton px-3 py-1 hover:bg-accent hover:text-accent-foreground flex items-center gap-3 [&_svg]:size-5! [&_svg]:text-popover-foreground ${className}`}
+      className={`FloatingMenuButton px-2 py-0.5 hover:bg-accent hover:text-accent-foreground flex items-center gap-1 [&_svg]:size-5! [&_svg]:text-popover-foreground ${className}`}
       type="button"
       {...props}
     >
