35304a3162228283e6688bb3255720653e849d53
roman-smolnyk <poma23324@gmail.com>
2026-06-23 08:00:35 +0200

    WIP

--- src/hooks/useUpdateVersion.tsx
+++ src/hooks/useUpdateVersion.tsx
@@ -9,9 +9,15 @@ export default function useUpdateVersion() {
         if (!navigator.onLine) return;
 
         try {
-          console.debug("useUpdateVersion:fetch");
-          const response = await fetch("/version.json");
+          // Add cache-busting param
+          const url = new URL("/version.json", window.location.origin);
+          url.searchParams.set("v", String(Date.now()));
+
+          const response = await fetch(url.toString());
           const data = await response.json();
+
+          console.debug("useUpdateVersion:fetch", data);
+
           setUpdateVersion(data.version);
         } catch (error) {
           console.error(error);
