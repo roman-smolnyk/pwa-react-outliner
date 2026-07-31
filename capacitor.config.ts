20a41caccd5db21a85ce58930f84eab222f74c43
roman-smolnyk <poma23324@gmail.com>
2026-07-31 09:19:44 +0200

    [feat] Add Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ua.pp.romansmolnyk.outliner',
+  appName: 'RS Outliner',
+  webDir: 'dist'
+};
+
+export default config;
